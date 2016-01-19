cee8ef95f3f943834ce388856bc0c8e1978aa4ce
sivaakurati <sivaakurati@gmail.com>
2016-01-19 13:30:09 +0530

    Update SubjectRepository.java

--- src/main/java/com/enuminfo/school/hibernate/repository/SubjectRepository.java
+++ src/main/java/com/enuminfo/school/hibernate/repository/SubjectRepository.java
@@ -12,4 +12,5 @@ import com.enuminfo.school.hibernate.model.Subject;
  */
 public interface SubjectRepository extends PagingAndSortingRepository<Subject, Integer> {
 
+	Subject findBySubjectName(String subjectName);
 }
