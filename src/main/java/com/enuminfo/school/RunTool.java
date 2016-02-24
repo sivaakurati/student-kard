09405069db4be868a762e51e3b86da44c939f937
sivaakurati <sivaakurati@gmail.com>
2016-02-24 10:40:44 +0100

    Update RunTool.java

--- src/main/java/com/enuminfo/school/RunTool.java
+++ src/main/java/com/enuminfo/school/RunTool.java
@@ -4,15 +4,19 @@
 package com.enuminfo.school;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Kumar
  */
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class RunTool {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunTool.class, args);
+		SpringApplication.run(RunTool.class);
 	}
 }
