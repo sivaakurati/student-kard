0f42b9d152b40049a565364156f6267efbc9cc5a
Kumar <Kumar@Kumar-PC>
2016-04-04 22:15:45 +0530

    updated html files with common lib path files to import html page

--- src/main/java/com/enuminfo/student/RunTool.java
+++ src/main/java/com/enuminfo/student/RunTool.java
@@ -5,17 +5,24 @@ package com.enuminfo.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Kumar
  */
-@Configuration
+@SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
-public class RunTool {
+public class RunTool extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		 return application.sources(RunTool.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RunTool.class);
 	}
